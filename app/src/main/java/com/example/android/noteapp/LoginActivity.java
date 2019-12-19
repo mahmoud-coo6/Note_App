dd61d6d57aa12ecdd2aa8be3037aef6c81ee1af8
Mahmoud Abdo <mahmoudcoo6@gmail.com>
2019-12-19 14:58:30 +0200

    start Database

--- app/src/main/java/com/example/android/noteapp/LoginActivity.java
+++ app/src/main/java/com/example/android/noteapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,10 +17,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     TextView text_singinTv, forgetTv;
@@ -108,10 +116,31 @@ public class LoginActivity extends AppCompatActivity {
         mAuth.signInWithEmailAndPassword(email, password).
                 addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    public void onComplete(@NonNull final Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
                             Toast.makeText(LoginActivity.this, "Success Login.", Toast.LENGTH_SHORT).show();
+
+                            Map<String,Object> data = new HashMap<>();
+                            data.put("lastSignIn",new Date().getTime());
+
+                            FirebaseDatabase.getInstance().getReference().child("User").child(user.getUid()).updateChildren(data)
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                            Log.d("error",e.getLocalizedMessage());
+                                        }
+                                    })
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Intent intent = new Intent(LoginActivity.this , MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    });
+
+
                             Intent intent = new Intent(LoginActivity.this, home_pages.class);
                             startActivity(intent);
 
