909ff9165bf6e216b150f6df2a9331b335b19a1f
Mahmoud Abdo <mahmoudcoo6@gmail.com>
2019-12-24 00:04:03 +0200

    finish chack android project

---
+++ app/src/main/java/com/example/android/noteapp/NoteSelectColor.java
@@ -0,0 +1,100 @@
+package com.example.android.noteapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.devs.vectorchildfinder.VectorChildFinder;
+import com.devs.vectorchildfinder.VectorDrawableCompat;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NoteSelectColor extends AppCompatActivity {
+
+    LinearLayout container;
+    int color;
+    Intent intent;
+    String categoryId;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_select_color);
+
+        container = findViewById(R.id.select_color_container);
+        final TextView title = findViewById(R.id.title);
+        final TextView description = findViewById(R.id.description);
+        final CheckBox date = findViewById(R.id.date);
+
+        intent = getIntent();
+        categoryId= intent.getStringExtra("CategoryId");
+        title.setText(intent.getStringExtra("title"));
+        description.setText(intent.getStringExtra("description"));
+        color = intent.getIntExtra("color", 0);
+        DateFormat formatter = new SimpleDateFormat("MMMM dd , yyyy HH:mm");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(intent.getLongExtra("date",0));
+        date.setText(formatter.format(calendar.getTime()));
+        container.setBackgroundColor(color);
+
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> data = new HashMap<>();
+                data.put("lastUpdate", new Date().getTime());
+                data.put("color", color);
+
+
+                FirebaseDatabase.getInstance().getReference().child("Note").child(
+                        intent.getStringExtra("NoteId")).updateChildren(data)
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(NoteSelectColor.this, "update category color faild", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Intent intent = new Intent(NoteSelectColor.this, CategoryNote.class);
+                                intent.putExtra("CategoryId",categoryId);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+            }
+        });
+    }
+
+
+    private void resetColor(int color) {
+        container.setBackgroundColor(color);
+    }
+
+
+    public void selectColor(View view) {
+        VectorChildFinder vector = new VectorChildFinder(this, view.getResources().getIdentifier(view.getTag().toString(), "drawable", getPackageName()), (ImageView) view);
+        VectorDrawableCompat.VFullPath path1 = vector.findPathByName("path1");
+        color = path1.getFillColor();
+        resetColor(color);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/noteapp/ViewArtBoard.java
@@ -0,0 +1,84 @@
+package com.example.android.noteapp;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.android.noteapp.MyFirebaseController.getDatabaseReference;
+
+public class ViewArtBoard extends AppCompatActivity {
+
+    CategrayAdapter categrayAdapter;
+    List<Category> categoryList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.art_board);
+
+        if (isNetworkAvailable()) {
+            initData();
+        } else {
+            Toast.makeText(this, "there is no interent connection.", Toast.LENGTH_SHORT).show();
+        }
+
+        int columns = 4;
+        RecyclerView recyclerView = findViewById(R.id.category_rv);
+        LinearLayoutManager layoutManager = new GridLayoutManager(this, columns);
+        recyclerView.setLayoutManager(layoutManager);
+        categrayAdapter = new CategrayAdapter(this, categoryList);
+        recyclerView.setAdapter(categrayAdapter);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    private void initData() {
+        DatabaseReference scoresRef =  getDatabaseReference().child("Category");
+        scoresRef.keepSynced(true);
+        getDatabaseReference().child("Category")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        categoryList.clear();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                            Category category = snapshot.getValue(Category.class);
+                            categoryList.add(category);
+
+                        }
+                        categrayAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+}
